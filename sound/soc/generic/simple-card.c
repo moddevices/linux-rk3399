6f85a0d4a85f76de5199d066e26f7721174aa598
falkTX <falktx@falktx.com>
2020-05-30 13:38:36 +0100

    Use clock #2 for codec dai
    Signed-off-by: falkTX <falktx@falktx.com>

--- sound/soc/generic/simple-card.c
+++ sound/soc/generic/simple-card.c
@@ -91,7 +91,7 @@ static int asoc_simple_card_hw_params(struct snd_pcm_substream *substream,
 
 	if (mclk_fs) {
 		mclk = params_rate(params) * mclk_fs;
-		ret = snd_soc_dai_set_sysclk(codec_dai, 0, mclk,
+		ret = snd_soc_dai_set_sysclk(codec_dai, 2, mclk,
 					     SND_SOC_CLOCK_IN);
 		if (ret && ret != -ENOTSUPP)
 			goto err;
